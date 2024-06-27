d3ccdb967998c68b7d56a4dcdfcfdf955198c08c
Billy Wallace <wallace.billy1@gmail.com>
2024-06-27 11:25:03 -0600

    model testing

---
+++ spec/models/tea_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe Tea do
+  describe 'validations' do
+    it { should validate_presence_of(:title) }
+    it { should validate_presence_of(:description) }
+    it { should validate_presence_of(:temperature) }
+    it { should validate_presence_of(:brew_time) }
+    it { should validate_numericality_of(:temperature) }
+    it { should validate_numericality_of(:brew_time) }
+  end
+end
